70664b24f528421145ac7fb7e1745efe6b8fd48b
Billy F <willfira717@gmail.com>
2020-12-23 14:23:20 -0500

    added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+driver_1 = Driver.find_or_create_by(name: "Speed Racer") 
+# driver_2 = Driver.create(name: "Racer X")
+# driver_3 = Driver.create(name: "Magilla Gorilla")
+# driver_4 = Driver.create(name: "Batman")
+
+car_1 = Car.find_or_create_by(model: "Mach 5", year: 1967, driver: driver_1)
